2ee79a688b2f5d626c9a8b89106e48a01febc6db
tatsujin-craft <tatsujin.robotics@gmail.com>
2024-10-14 00:12:39 +0900

    add: 028 DP frog jump

--- algorithm_math_problems/scripts/028.py
+++ algorithm_math_problems/scripts/028.py
@@ -1,36 +1,47 @@
 #!/usr/bin/env python3
 """
-Description: 
-    Two Cards
-    This script finds the pair of p and q whose sum is k.
-
+Description: DP Frog jump
 Author: tatsujin
-Date: 2024-08-17
+Date: 2024-10-13
 """
 
+DEBUG_LOG_ENABLED = True
 
-def find_target_pair():
-    _, k = map(int, input().split())
-    p_list = [int(p) for p in input().split()]
-    q_list = [int(q) for q in input().split()]
-    is_exist = False
 
-    for p in p_list:
-        for q in q_list:
-            if p + q == k:
-                is_exist = True
-                break
+def get_result_with_dp(N, H):
+    dp = [float("inf")] * (N + 1)
+    dp[1] = 0
+    if DEBUG_LOG_ENABLED:
+        print(f"Height of steps: {H}")
+        print(f"Initial dp: {dp}\n")
 
-    if is_exist:
-        print("Yes")
-        print(f"Found pair: p={p}, q={q}, k={k}")
+    for i in range(2, N + 1):
+        # Get minimum cost of jumping ahead one (i+1)
+        dp[i] = min(dp[i], dp[i - 1] + abs(H[i - 2] - H[i - 1]))
+        if DEBUG_LOG_ENABLED:
+            print(f"Jump+1, step{i-1} => step{i}, dp[{i}] = {dp[i]}")
 
-    else:
-        print("No")
+        if i > 2:
+            # Get minimum cost of jumping ahead two (i+2)
+            dp[i] = min(dp[i], dp[i - 2] + abs(H[i - 3] - H[i - 1]))
+            if DEBUG_LOG_ENABLED:
+                print(f"Jump+2, step{i-2} => step{i}, dp[{i}] = {dp[i]}")
+
+        if DEBUG_LOG_ENABLED:
+            print(f"Current position: step{i}, dp: {dp}\n")
+
+    return dp[N]
 
 
 def main():
-    find_target_pair()
+    N = int(input())
+    H = list(map(int, input().split()))
+
+    cost = get_result_with_dp(N, H)
+    if DEBUG_LOG_ENABLED:
+        print(f"Minimum total cost: dp[{N}] = {cost}")
+    else:
+        print(cost)
 
 
 if __name__ == "__main__":
